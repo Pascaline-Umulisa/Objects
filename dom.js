b93cf0e38f84ae4ddd7ad85e281567fed5a8536a
Pascaline-Umulisa <kayiteteumulisa@gmail.com>
2022-07-27 17:06:20 +0300

    added a box shadow

--- dom.js
+++ dom.js
@@ -30,19 +30,19 @@ image.style.width="37%"
 image.style.top="0"
 image.style.left="0"
 image.style.opacity="0.7"
-body.style.padding="5% 0 0 40%"
+body.style.padding="5% 0 0 0"
 body.style.fontSize="20px"
+// body.style.backgroundImage='crop.png'
 
+let container=document.querySelector("article")
+container.style.margin="0 0 0 45%"
+// container.style.backgroundImage="url(fruits.png)"
+container.style.backgroundRepeat="no-repeat"
+container.style.backgroundSize="cover"
+container.style.backgroundBlendMode="darken"
+container.style.width="30%"
+container.style.padding="2%"
+container.style.borderRadius="15px"
+container.style.boxShadow="1px 1px 30px green"
 
-let newContainer=document.createElement("article")
-
-body.children.innerHTML.forEach(item=>
-       console.log(item)
-    // newContainer.appendChild(item) 
-//    body.appendChild(newContainer)
-    )
-//    newContainer.appendChild(item) 
-//    body.appendChild(newContainer)
-
-//    console.log(newContainer)
 
